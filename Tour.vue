273e2dde4f3faf0cfb2d304485eb97e4f5a50838
Anne L'Hôte <anne.lhote@gmail.com>
2020-09-30 12:26:11 +0000

    Remove titles

--- Tour.vue
+++ Tour.vue
@@ -9,15 +9,9 @@ export default {
     return {
       steps: [{
         target: '.project-cards__item:nth-child(1)',
-        header: {
-          title: 'First step',
-        },
         content: 'Enter a project like the LuxLeaks!'
       }, {
         target: '.search-layout-selector__button:nth-child(3)',
-        header: {
-          title: 'Second step',
-        },
         content: 'Use different views: List, Grid and Table',
         before: _ => new Promise(resolve => resolve(this.$router.push({ name: 'search' })))
       }]
