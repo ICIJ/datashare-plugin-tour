2ab73cc2b60a41591655bd1923319587953c65f3
Anne L'Hôte <anne.lhote@gmail.com>
2020-10-01 16:38:45 +0000

    Display multiple popover

--- Step.vue
+++ Step.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="text-center my-3">
-    <b-popover :target="target" triggers="hover" :placement="placement" ref="popover">
+    <b-popover :target="steps[0].target" :placement="steps[0].placement" ref="popover-0" v-if="steps.length">
       <template v-slot:title>
-        <span v-html="title"></span>
+        <span v-if="steps[0].title" v-html="steps[0].title"></span>
       </template>
-      <span v-html="content"></span>
+      <span v-if="steps[0].content" v-html="steps[0].content"></span>
+    </b-popover>
+    <b-popover :target="steps[1].target" :placement="steps[1].placement" ref="popover-1" v-if="steps[1]">
+      <template v-slot:title>
+        <span v-if="steps[1].title" v-html="steps[1].title"></span>
+      </template>
+      <span v-if="steps[1].content" v-html="steps[1].content"></span>
     </b-popover>
   </div>
 </template>
@@ -14,16 +20,24 @@ export default {
   name: 'Popover',
   data () {
     return {
-      target: null,
-      title: 'First step',
-      content: 'This is my content',
-      placement: 'top'
+      steps: []
     }
   },
   async mounted () {
-    const target = document.querySelector('.project-cards__item')
-    await this.$set(this, 'target', target)
-    this.$refs.popover.$emit('open')
+    const steps = [{
+      target: document.querySelector('.project-cards__item'),
+      title: 'First step',
+      content: 'This is my content',
+      placement: 'top'
+    }, {
+      target: document.querySelector('.search-bar__submit'),
+      title: 'Second step',
+      content: 'This is another content',
+      placement: 'top'
+    }]
+    await this.$set(this, 'steps', steps)
+    this.$refs['popover-0'].$emit('open')
+    this.$refs['popover-1'].$emit('open')
   }
 }
 </script>
