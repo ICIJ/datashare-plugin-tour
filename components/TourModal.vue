0ff6553ff9136bcd07ea2af80e99b35da1bc0ed7
Pierre Romera <hello@pirhoo.com>
2025-04-25 16:40:14 +0000

    feat: add tour modal creation/deletion icij/datashare#1618

---
+++ components/TourModal.vue
@@ -0,0 +1,25 @@
+<script setup>
+import img from '@/assets/tour-modal-light.svg'
+import imgDark from '@/assets/tour-modal-dark.svg'
+import { useCoreComponent } from '@/composables/useCoreComponent'
+
+const AppModal = await useCoreComponent('AppModal/AppModal')
+const ButtonIcon = await useCoreComponent('Button/ButtonIcon')
+const ImageMode = await useCoreComponent('ImageMode/ImageMode')
+const ImageModeSource = await useCoreComponent('ImageMode/ImageModeSource')
+</script>
+
+<template>
+  <app-modal title="Discover Datashare's main features" no-header-close>
+    <image-mode class="d-block text-center">
+      <image-mode-source :src="img" />
+      <image-mode-source :src="imgDark" color-mode="dark" />
+    </image-mode>
+    <template #footer="{ cancel, ok }">
+      <div class="d-flex justify-content-between w-100">
+        <button-icon variant="outline-secondary" @click="cancel">Skip tour</button-icon>
+        <button-icon variant="action" icon-right="shooting-star" @click="ok">Start</button-icon>
+      </div>
+    </template>
+  </app-modal>
+</template>
