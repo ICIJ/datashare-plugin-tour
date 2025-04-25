99748282863a30504a3f68da5239d19bcf6c3449
Pierre Romera <hello@pirhoo.com>
2025-04-25 16:40:52 +0000

    feat: add tour popover description icij/datashare#1618

---
+++ components/TourStepPopoverDescription.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  step: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <ul v-if="Array.isArray(step.popover.description)">
+    <li v-for="(item, index) in step.popover.description" :key="index">
+      {{ item }}
+    </li>
+  </ul>
+  <p v-else>{{ step.popover.description }}</p>
+</template>
