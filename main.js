860da91ca5a741dc8dcb2777dc1cd7af4bd4f7d0
Pierre Romera <hello@pirhoo.com>
2025-04-25 16:42:29 +0000

    feat: register hooks icij/datashare#1618

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-import Tour from './Tour.vue'
+import Tour from '@/components/Tour.vue'
+import TourStart from '@/components/TourStart.vue'
 
-document.addEventListener('datashare:ready', async ({ detail }) => {
+document.addEventListener(
+  'datashare:ready',
+  async ({ detail }) => {
+    detail.core.registerHook({
+      target: 'app:after',
+      definition: Tour
+    })
 
-  detail.core.registerHook({
-    target: 'app:after',
-    definition: Tour
-  })
-
-}, false)
+    detail.core.registerHook({
+      target: 'app-sidebar-footer-links-entry:after',
+      definition: TourStart
+    })
+  },
+  false
+)
