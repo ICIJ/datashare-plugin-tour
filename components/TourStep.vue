1dcf11bbab0338cf10ca526b499096516b44f062
Pierre Romera <hello@pirhoo.com>
2025-04-25 16:41:50 +0000

    feat: add tour step icij/datashare#1618
    This component only support popover yet but we could imagine creating more component for each stetps in future.

---
+++ components/TourStep.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TourStepPopover from '@/components/TourStepPopover.vue'
+
+defineProps({
+  step: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <tour-step-popover :step="step" />
+</template>
